085e0be8aa676c7e07c825629ade24aec28fe202
MOE1234moe <168225212+MOE1234moe@users.noreply.github.com>
2024-05-07 20:41:12 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+const config = require("./config.js");
+const Discord = require("discord.js");
+//exports.token = "";
+//exports.mongodb = "";
+
+
+exports.owners = ["1106672364063817868","","1184768077997547544","1106672364063817868"];
+
+// colors
+exports.ImageColors = "https://media.discordapp.net/attachments/1084557877802438717/1088531232524079214/IMG_8908.jpg" // صوره الالوان
+exports.Text = "colors list" // كلام على الصوره
+exports.Textcolor = "#f2f5f4" // لون النص لي على الصوره
+
+
+
+
+exports.status = 'Onlien';
+
+exports.activity = {
+  name: 'Deve @ 2023<>',
+  type: 'STREAMING',
+  url: 'https://www.twitch.tv/sfsf13'
+};
